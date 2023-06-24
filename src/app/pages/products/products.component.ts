bf43b75df0d63a0397f3fe3a57cc7ea3942b5886
PseudoElement <bubamainer@mail.ru>
2023-06-24 05:35:47 +0300

    test_rxjs_operators

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,9 +1,39 @@
 import { OnChanges, OnInit, SimpleChanges } from "@angular/core";
 import { Component, Input } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import {
+     BehaviorSubject,
+     Observable,
+     Subject,
+     catchError,
+     concatMap,
+     debounce,
+     debounceTime,
+     exhaustMap,
+     filter,
+     finalize,
+     find,
+     first,
+     from,
+     interval,
+     map,
+     mergeMap,
+     of,
+     switchMap,
+     take,
+     tap,
+} from "rxjs";
 import { ModalService } from "src/app/services/modal.service";
 import { ProductsService } from "src/app/services/product.service";
 import { Product } from "src/app/shared/types/products";
+import { ajax } from "rxjs/ajax";
+
+interface IRes {
+     completed: boolean;
+     id: number;
+     title: string;
+     userId: number;
+}
 
 @Component({
      selector: "app-products",
@@ -13,15 +43,45 @@ import { Product } from "src/app/shared/types/products";
 export class ProductsComponent implements OnInit, OnChanges {
      title = "AngularApp";
      // private route: ActivatedRoute;
+     id = 1;
      @Input() name: string = "NAME";
      @Input() products: Product[] = this.productService.products;
      isLoading = false;
      // products$: Observable<Product[]>;
+     obs$ = new BehaviorSubject(this.name);
 
      constructor(
           public productService: ProductsService,
           public modalService: ModalService
-     ) {}
+     ) {
+          from([1, 2, 3, 4, 5, 6])
+               .pipe(
+                    mergeMap((id) =>
+                         ajax.getJSON<IRes>(
+                              `https://jsonplaceholder.typicode.com/todos/${id}`
+                         )
+                    ),
+                    filter((val) => val.id > 2),
+                    map((val) => ({ ...val, myKey: "new-key" })),
+                    tap({
+                         complete() {
+                              console.log("TAP COMPLETED");
+                         },
+                    }),
+                    catchError((e) => {
+                         console.log("ERROR", e);
+                         return of(e);
+                    }),
+                    debounceTime(10000),
+                    finalize(() => console.log("FINAL")),
+                    take(5)
+               )
+               .subscribe((data) => {
+                    this.id++;
+                    console.log(data);
+               });
+          this.obs$.pipe(debounceTime(500)).subscribe(console.log);
+     }
 
      changeName(): void {
           this.name += "1";
@@ -48,7 +108,7 @@ export class ProductsComponent implements OnInit, OnChanges {
           console.log("CHANGES", changes);
      }
 
-     onNgModelChange(e: Event) {
-          console.log(this.name);
+     onNgModelChange(name: string) {
+          this.obs$.next(name);
      }
 }
