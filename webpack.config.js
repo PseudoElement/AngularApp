faece64d8af86a97e3f8d82ca82be42f491210c0
pseudoelement <bubamainer@mail.ru>
2024-01-31 21:08:02 +0300

    create custom webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const path = require('path');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                // Copy the assets folder to the output directory
+                { from: 'src/assets', to: 'assets' },
+            ],
+        }),
+    ],
     output: {
-        filename: 'bundle.js',
+        filename: 'bundle-1.js',
         path: path.resolve(__dirname, 'dist'),
     },
     resolve: {
